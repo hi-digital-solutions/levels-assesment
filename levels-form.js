251bb17191e0df893187d9ce06069ab72204d4e3
ifirkin <ian.firkin@hidigital.solutions>
2020-03-20 16:55:01 -0400

    Adds sections for each question

--- levels-form.js
+++ levels-form.js
@@ -1,13 +1,24 @@
 
 var form = FormApp.create('DS Levels Assessment');
 
-function addQuestion() {
+function addQuestion(questionText) {
+    var section = form.addPageBreakItem();
+    section.setHelpText(questionText)
     form.addScaleItem()
-        .setTitle('Test')
+        .setTitle('... individual work')
+        .setBounds(1, 5)
+        .setLabels('Strongly Disagree', 'Strongly Agree')
+    form.addScaleItem()
+        .setTitle('... team work and dynamics')
+        .setBounds(1, 5)
+        .setLabels('Strongly Disagree', 'Strongly Agree')
+    form.addScaleItem()
+        .setTitle('... organizational')
         .setBounds(1, 5)
         .setLabels('Strongly Disagree', 'Strongly Agree')
 }
 
 function main() {
-    addQuestion();
+    addQuestion('This person does great at ______');
+    addQuestion('For _____, this person is OK');
 }

5f96094b7980d76510c587ac8f7478c8b21500d4
ifirkin <ian.firkin@hidigital.solutions>
2020-03-23 07:53:58 -0400

    Adds real questions

--- levels-form.js
+++ levels-form.js
@@ -19,6 +19,18 @@ function addQuestion(questionText) {
 }
 
 function main() {
-    addQuestion('This person does great at ______');
-    addQuestion('For _____, this person is OK');
+    addQustion('You leave things better than you found them');
+    addQustion('Others come to you for direction or guidance');
+    addQustion('Others enjoy working with you, and want you on their team');
+    addQustion('You demonstrate improvement');
+    addQustion('You bring the right people into the organization and/or team');
+    addQustion('You reframe problems to come up with better solutions');
+    addQustion('You demonstrate the ability to deliver value quickly, and maximize work not done');
+    addQustion('You know when to build consensus, and can recognize premature convergence');
+    addQustion('You have conversations at the right level of detail, and bring clarity to discussions');
+    addQustion('You think about total cost of ownership, and optimize for overall value');
+    addQustion('You design simple systems');
+    addQustion('You design systems to remove business constraints');
+    addQustion('You can manage time and logistics');
+    addQustion('You are proactive in doing the right thing without being asked; you take ownership');
 }

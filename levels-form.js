a50b0c2616149ea33285e54f22d59f09e25bb808
ifirkin <ian.firkin@hidigital.solutions>
2020-03-23 10:18:53 -0400

    Changes to questions after first test run

--- levels-form.js
+++ levels-form.js
@@ -6,23 +6,37 @@ form.getItems().map(x => form.deleteItem(x));
 function addQuestion(questionText) {
     form.addGridItem()
         .setTitle(questionText)
-        .setRows(['For individual work', 'At the team level', 'At the organizational level'])
+        .setRows(['At the individual level', 'At the team level', 'At the organizational level'])
         .setColumns(['Almost Never', 'Seldom', 'Sometimes', 'Often', 'Almost Always']);
 }
 
 function main() {
+    var item = form.addListItem();
+    item.setTitle('Who are you evaluating?')
+        .setChoices([
+            item.createChoice('***REMOVED***'),
+            item.createChoice('***REMOVED***'),
+            item.createChoice('***REMOVED***'),
+            item.createChoice('***REMOVED***'),
+            item.createChoice('***REMOVED***'),
+            item.createChoice('***REMOVED***'),
+            item.createChoice('***REMOVED***'),
+            item.createChoice('***REMOVED***'),
+            item.createChoice('***REMOVED***'),
+            item.createChoice('***REMOVED***')
+         ])
     addQuestion('They leave things better than they found them');
     addQuestion('Others come to them for direction or guidance');
-    addQuestion('Others enjoy working with them, and want them on their team');
-    addQuestion('They demonstrates improvement');
-    addQuestion('They bring the right people into the organization');
+    addQuestion('Others enjoy working with them, and want them on their team'); // Does this make sense at individual/team/org?
+    addQuestion('They demonstrate improvement'); // Does this make sense at i/t/o?
+    addQuestion('They bring the right people into the organization'); // participating in interviews, working on interview process, growing org culture
     addQuestion('They reframe problems to come up with better solutions');
     addQuestion('They deliver value quickly, and maximize work not done');
-    addQuestion('They know when to build consensus, and can recognize premature convergence');
+    addQuestion('They build consensus, and avoid premature convergence');
     addQuestion('They have conversations at the right level of detail, and bring clarity to discussions');
     addQuestion('They think about total cost of ownership, and optimize for overall value');
-    addQuestion('They design simple systems');
-    addQuestion('They design systems to remove business constraints');
+    addQuestion('They create simple systems');
+    addQuestion('They create systems to remove business constraints');
     addQuestion('They can manage time and logistics');
-    addQuestion('They are proactive in doing the right thing without being asked; they take ownership');
+    addQuestion('They do the right thing without being asked');
 }

4467e527b9be37976f7ed1e9b33d6a33e6a449df
ifirkin <ian.firkin@hidigital.solutions>
2020-03-23 08:11:33 -0400

    Changes question text

--- levels-form.js
+++ levels-form.js
@@ -1,7 +1,7 @@
 
 var formId = '1MRjM3YSFTB3xtxJ1s1uRn1_O7p9NWpJXVwRD-paX6Xc';
 var form = FormApp.openById(formId);
-form.getItems().map(form.deleteItem);
+form.getItems().map(x => form.deleteItem(x));
 
 function addQuestion(questionText) {
     form.addGridItem()
@@ -11,18 +11,18 @@ function addQuestion(questionText) {
 }
 
 function main() {
-    addQustion('You leave things better than you found them');
-    addQustion('Others come to you for direction or guidance');
-    addQustion('Others enjoy working with you, and want you on their team');
-    addQustion('You demonstrate improvement');
-    addQustion('You bring the right people into the organization and/or team');
-    addQustion('You reframe problems to come up with better solutions');
-    addQustion('You demonstrate the ability to deliver value quickly, and maximize work not done');
-    addQustion('You know when to build consensus, and can recognize premature convergence');
-    addQustion('You have conversations at the right level of detail, and bring clarity to discussions');
-    addQustion('You think about total cost of ownership, and optimize for overall value');
-    addQustion('You design simple systems');
-    addQustion('You design systems to remove business constraints');
-    addQustion('You can manage time and logistics');
-    addQustion('You are proactive in doing the right thing without being asked; you take ownership');
+    addQuestion('They leave things better than they found them');
+    addQuestion('Others come to them for direction or guidance');
+    addQuestion('Others enjoy working with them, and want them on their team');
+    addQuestion('They demonstrates improvement');
+    addQuestion('They bring the right people into the organization');
+    addQuestion('They reframe problems to come up with better solutions');
+    addQuestion('They deliver value quickly, and maximize work not done');
+    addQuestion('They know when to build consensus, and can recognize premature convergence');
+    addQuestion('They have conversations at the right level of detail, and bring clarity to discussions');
+    addQuestion('They think about total cost of ownership, and optimize for overall value');
+    addQuestion('They design simple systems');
+    addQuestion('They design systems to remove business constraints');
+    addQuestion('They can manage time and logistics');
+    addQuestion('They are proactive in doing the right thing without being asked; they take ownership');
 }
